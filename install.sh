091d3d1f9242d04d4103c24a5d22e6de94941054
TheDriver <110929811+Hoolaf@users.noreply.github.com>
2025-03-17 11:30:39 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
 
-# Script d'installation automatisée d'un NAS Debian
-# Ce script configure un serveur NAS complet sous Debian 12
-# avec RAID 5, SFTP, WebDAV, Samba, Webmin et gestion des utilisateurs
-
-# Vérification des privilèges
 if [ "$(id -u)" -ne 0 ]; then
     echo "Ce script doit être exécuté en tant que root ou avec sudo."
     exit 1
 fi
 
-# Définition des couleurs
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 NC='\033[0m'
 
-# Fonctions d'affichage
 display_message() {
     echo -e "${GREEN}[INFO]${NC} $1"
 }
